3a65133c3692e80d484b2a60d160b48153a2ff30
Nilton da Silva Junior <tonsti@gmail.com>
2020-12-06 19:17:06 +0100

    Proyect Created

---
+++ backend/src/test/java/com/nilton/movieflix/MovieFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilton.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
