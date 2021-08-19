805a026557fa54440adacb53ef59e2af4e87bcc3
Nilton da Silva Junior <tonsti@gmail.com>
2021-08-19 21:03:53 +0200

    Add Login Page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,7 +1,28 @@
+import './styles.css';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+
 const Login = () => {
-    return (
-        <h1>Login Page</h1>
-    );
-}
+  return (
+    <div className="login-container container-fluid">
+      <div className="login-content-container">
+        <h1>Avalie Filmes</h1>
+        <p>
+          Diga o que você achou do seu <br /> filme favorito
+        </p>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
+      </div>
+      <div className="login-card-container base-card">
+        <h1>LOGIN</h1>
+        <form className="login-form-container">
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Senha" />
+          <button className="btn btn-primary" type="button">FAZER LOGIN</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
