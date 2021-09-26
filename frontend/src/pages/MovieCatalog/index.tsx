620c0c1be89209f20abcdc0cb1eadf8d928896fe
Nilton da Silva Junior <tonsti@gmail.com>
2021-09-26 21:42:34 +0200

    Refactor getMovies in MovieCatalog using useCallBack

--- frontend/src/pages/MovieCatalog/index.tsx
+++ frontend/src/pages/MovieCatalog/index.tsx
@@ -1,7 +1,7 @@
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Movie } from 'types/movie';
 import { SpringPage } from 'types/vendor/spring';
 import { requestBackend } from 'util/requests';
@@ -27,21 +27,13 @@ const MovieCatalog = () => {
 
   const { handleSubmit, control } = useForm<Genre>();
 
-  useEffect(() => {
-    getMovies(0);
-  }, []);
-
-  useEffect(() => {
-    getGenres();
-  }, []);
-
-  const getMovies = (pageNumber: number) => {
+  const getMovies = useCallback(() => {
     const params: AxiosRequestConfig = {
       method: 'GET',
       url: '/movies',
       withCredentials: true,
       params: {
-        page: pageNumber,
+        page: controlComponentsData.activePage,
         linesPerPage: 4,
       },
     };
@@ -49,7 +41,17 @@ const MovieCatalog = () => {
     requestBackend(params).then((response) => {
       setPage(response.data);
     });
-  };
+  },[controlComponentsData.activePage]);
+
+  useEffect(() => {
+    getMovies();
+  }, [getMovies]);
+
+  useEffect(() => {
+    getGenres();
+  }, []);
+
+
 
   const getGenres = () => {
     const params: AxiosRequestConfig = {
@@ -61,6 +63,10 @@ const MovieCatalog = () => {
     requestBackend(params).then((response) => {
       setSelectGenre(response.data);
     });
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({activePage: pageNumber})
   }
 
   const onSubmit = (formData: Genre) => {
@@ -108,7 +114,7 @@ const MovieCatalog = () => {
       <Pagination
         pageCount={page ? page?.totalPages : 0}
         range={3}
-        onChange={getMovies}
+        onChange={handlePageChange}
       />
     </div>
   );
